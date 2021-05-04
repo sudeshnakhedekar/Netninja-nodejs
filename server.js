d036598e57da7aa1f8910fe7393f482044df7c2d
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-04 11:16:10 +0530

    server created

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const hhtp = require('http');
 
-
 //creates a server
 const server = hhtp.createServer((request , response) =>{
- console.log('request made');
+ console.log('request made..');
 });
 
 //listening to port 3000 on localhost
 server.listen(3000, 'localhost' , ()=>{
-    console.log('listening fo request on port 3000');
+    console.log('Listening for request on port 3000');
 });
 
-//lsend request to browser
+
+
