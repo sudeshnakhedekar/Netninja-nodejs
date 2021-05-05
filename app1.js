2b44bb653dbaf70d3905ccc3efb420f39cc24bb0
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-05 11:29:23 +0530

    redirect and 404

--- app1.js
+++ app1.js
@@ -18,6 +18,17 @@ const express = require('express');
     response.sendFile('./views/about.html',{root : __dirname});
 });
 
+//redirect
+app.get('/about-us' ,(request, response) =>{
+  response.redirect('./about');
+});
+
+//404 file
+app.use((request, response) =>{
+
+  response.status(404).sendFile('./views/404.html',{root : __dirname});
+});
+
 
 
 
