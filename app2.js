3143d78de621ab70f365c01f734d1b384fc8f76e
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-05 13:49:48 +0530

    passing data into views

--- app2.js
+++ app2.js
@@ -14,21 +14,26 @@ const express = require('express');
   app.listen(3000);
 
   app.get('/' ,(request, response) =>{
-   response.render('index')
+    const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem inpsum dolor sit amet consectrtur'},
+      {title: 'Mario finds stars', snippet: 'Lorem inpsum dolor sit amet consectrtur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem inpsum dolor sit amet consectrtur'},
+  ];
+   response.render('index',{title :'Home',blogs})
   });
 
   app.get('/about' ,(request, response) =>{
     // response.send('<p>About Page</p>')
-    response.render('about')
+    response.render('about' ,{title :'About'})
 });
 
 app.get('/blogs/create',(request, response) =>{
-    response.render('create')
+    response.render('create' ,{title :'Create a new Blog'})
 })
 
 //404 file
 app.use((request, response) =>{
 
-  response.status(404).render('404')
+  response.status(404).render('404' ,{title :'404'})
 });
 
