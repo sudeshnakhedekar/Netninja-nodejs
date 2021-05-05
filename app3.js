1b6bfb91229110fb6c24f4afa32e998a0b890536
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-05 20:36:14 +0530

    middleware

---
+++ app3.js
@@ -0,0 +1,46 @@
+//Concept of EJS
+
+const express = require('express');
+
+//express app
+ const app = express();
+
+ //register view engine
+ app.set('view engine','ejs');
+
+
+
+  //listen for requests
+  app.listen(3000);
+
+app.use((request, response)=>{
+console.log('new request made:');
+console.log('host: ' ,request.hostname);
+console.log('path: ',request.path);
+console.log('method: ',request.method);
+})
+
+  app.get('/' ,(request, response) =>{
+    const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem inpsum dolor sit amet consectrtur'},
+      {title: 'Mario finds stars', snippet: 'Lorem inpsum dolor sit amet consectrtur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem inpsum dolor sit amet consectrtur'},
+  ];
+   response.render('index',{title :'Home',blogs})
+  });
+
+  app.get('/about' ,(request, response) =>{
+    // response.send('<p>About Page</p>')
+    response.render('about' ,{title :'About'})
+});
+
+app.get('/blogs/create',(request, response) =>{
+    response.render('create' ,{title :'Create a new Blog'})
+})
+
+//404 file
+app.use((request, response) =>{
+
+  response.status(404).render('404' ,{title :'404'})
+});
+
